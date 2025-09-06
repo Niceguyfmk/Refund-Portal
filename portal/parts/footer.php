a89e2659ff2255ba22ac04398d8a4bb8bd3e7d3e
Faiz Khan <faizmdkhan@hotmail.com>
2025-09-06 21:30:32 +0530

    removed manifest

--- portal/parts/footer.php
+++ portal/parts/footer.php
@@ -2,7 +2,7 @@
     <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
     <script src="../assets/libs/aos/dist/aos.js"></script>
     <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.js"></script>
-    <script src="../assets/js/app.js"></script>
+    <!-- <script src="../assets/js/app.js"></script> -->
 </body>
 
 </html>
